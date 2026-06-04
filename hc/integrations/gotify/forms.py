b61d185ef8600f7dd013a2f948b9c57dad395890
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2026-06-04 18:34:09 -0300

    ensure priority values are clean in AddGotifyForm

--- hc/integrations/gotify/forms.py
+++ hc/integrations/gotify/forms.py
@@ -19,5 +19,13 @@ class AddGotifyForm(forms.Form):
         choices=PRIORITY_CHOICES, coerce=int, required=False, initial=5
     )
 
+    def clean_priority(self) -> int:
+        value = self.cleaned_data.get("priority")
+        return 5 if value is None else value
+
+    def clean_priority_up(self) -> int:
+        value = self.cleaned_data.get("priority_up")
+        return 5 if value is None else value
+
     def get_value(self) -> str:
         return json.dumps(dict(self.cleaned_data), sort_keys=True)
